2871f0f0ade401fca258b0c7dc0929c0b5ce79c8
supervsky <supervsky05@gmail.com>
2018-01-28 02:48:37 +0800

    to rewrite preload plugin

---
+++ page4/js/preload-0.0.js
@@ -0,0 +1,41 @@
+(function(){
+	// aImgs存放images的src属性值
+	var aImgs = [];
+	for(var i = 0; i < 5; i++){
+		aImgs[i] = "images/pic (" + (i+1) + ").jpg";
+		 // console.log(aImgs[i]);
+	}
+	
+	// index [0, len-1]
+	var index = 0, len = aImgs.length;
+
+	// 点击切换按钮
+	$('.btn').on('click', function(){
+		if($(this).data('control') === 'prev'){
+			// alert('上一张');
+			index = index <= 0 ? index : index-1;
+		} else {
+			// alert('下一张');
+			index = index < len-1 ? index+1 : index;
+		}
+		// console.info(index);
+		document.title = '相册' + (index + 1) + '/' + len;
+		$('.gallery-box img').attr('src', aImgs[index]);
+	});
+
+	//预加载
+	var count = 0;
+	$.each(aImgs, function(i, src){
+		var oImg = new Image();
+		$(oImg).on('load', function(){
+			count++;
+			$('.progress').html(Math.round(count / len * 100) + '%');
+			if(count >= len){
+				$('.loading').hide();
+				document.title = '1/'+len;
+			}
+		});
+		oImg.src = src;
+	});
+
+})();
--- page4/js/preload.js
+++ page4/js/preload.js
@@ -1,41 +1,37 @@
 (function(){
-	// aImgs存放images的src属性值
-	var aImgs = [];
-	for(var i = 0; i < 5; i++){
-		aImgs[i] = "images/pic (" + (i+1) + ").jpg";
-		 // console.log(aImgs[i]);
-	}
 	
-	// index [0, len-1]
-	var index = 0, len = aImgs.length;
+	//  variables declaration
+	var aImgs,	// images array
+		index,	// index of the current image
+		len;		// length of the images array
 
+	// variables initialization
+	aImgs = [ ];
+	for(var i = 0; i < 10; i++){ //init aImgs
+		aImgs[i] = "images/pic (" + (i+1) + ").jpg";
+	}
+	index = 0, 
+	len = aImgs.length;
+
+	// execute
+	$.preload(aImgs, {
+		each : function (count) {
+			$('.progress').html(Math.round(count / len * 100) + '%');
+		},
+		all :  function () {
+			$('.loading').hide();
+			document.title = '相册 1/'+len;
+		}
+	});
 	// 点击切换按钮
 	$('.btn').on('click', function(){
 		if($(this).data('control') === 'prev'){
-			// alert('上一张');
 			index = index <= 0 ? index : index-1;
 		} else {
-			// alert('下一张');
 			index = index < len-1 ? index+1 : index;
 		}
-		// console.info(index);
 		document.title = '相册' + (index + 1) + '/' + len;
 		$('.gallery-box img').attr('src', aImgs[index]);
-	});
-
-	//预加载
-	var count = 0;
-	$.each(aImgs, function(i, src){
-		var oImg = new Image();
-		$(oImg).on('load', function(){
-			count++;
-			$('.progress').html(Math.round(count / len * 100) + '%');
-			if(count >= len){
-				$('.loading').hide();
-				document.title = '1/'+len;
-			}
-		});
-		oImg.src = src;
 	});
 
 })();
