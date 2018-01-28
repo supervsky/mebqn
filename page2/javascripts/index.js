3de22a31019f50fdf0d78551867d8b3c2363e6ee
supervsky <supervsky05@gmail.com>
2018-01-28 11:39:39 +0800

    add preload images

---
+++ page2/javascripts/index.js
@@ -0,0 +1,29 @@
+(function(){
+	//  variables declaration
+	var aImgs,	// objects array of images
+		imgs,	// strings array of images 
+		len;		// length of the images array
+
+	// variables initialization
+	aImgs = document.getElementsByTagName('img');
+	imgs = [ ];
+	len = aImgs.length;
+	for(var i = 0; i < len; i++) {
+		imgs.push(aImgs[i].src)
+	}
+
+	// execute
+	$.preload(imgs, {
+		each : function (count) {
+			var percentage = Math.round(count / len * 100) + '%';
+			console.log(percentage);
+			$('.progress-bar').html( percentage );
+			$('.progress-bar').attr('style', function(){
+				return 'width:' + percentage;
+			});
+		},
+		all :  function () {
+			$('.box').hide();
+		}
+	});
+})();
---
+++ page2/javascripts/preload-1.0.js
@@ -0,0 +1,39 @@
+// 图片预加载
+(function($){ 
+
+	function Preload(imgs, options){
+		this.imgs = typeof imgs === 'string' ? [ imgs ] : imgs;
+		this.opts = $.extend ({ }, Preload.DEFAULTS, options);
+		this._unordered();
+	}
+
+	Preload.DEFAULTS = {
+		each : null,  // execute after each img loaded
+		 all : null	// execute after each img loaded
+	};
+
+	Preload.prototype._unordered = function () {
+		var imgs = this.imgs;
+		var opts = this.opts;
+		var len = imgs.length;
+		var count = 0;
+		$.each(imgs, function(i, src){
+			if (typeof src != 'string') return;  
+			var oImg = new Image();
+			$(oImg).on('load', function(){
+				count++;
+				opts.each && opts.each(count);
+				if(count >= len){
+					opts.all && opts.all();
+				}
+			});
+			oImg.src = src;
+		});
+	};
+
+	$.extend({
+		preload: function(imgs, opts){
+			return new Preload(imgs, opts);
+		}
+	});
+})(jQuery);
